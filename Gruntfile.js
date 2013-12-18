e9a5430f238cccd609603e35e3c9c9511ed088b7
Eric Vantillard <eric.vantillard@evaxion.fr>
2013-12-18 14:01:02 +0100

    Add bower,grunt for bower and clean tasks, and init template

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+
+var path = require('path');
+var _ = require("underscore");
+
+module.exports = function( grunt ) {
+    'use strict';
+
+    grunt.initConfig({
+      bower: {
+        install: {
+          options: {
+            targetDir: './src/files/vendor',
+            layout: function(type, component) {
+                var realType=type;
+                var minSuffix = type.indexOf('-min');
+                var minified = false;
+                if(minSuffix!=-1){
+                    realType = type.substring(0,minSuffix);
+                    minified = true;
+                }
+                return path.join(component,realType);
+            },
+            install: true,
+            verbose: false,
+            cleanTargetDir: true,
+            cleanBowerDir: false,
+            bowerOptions: {}
+          }
+        }
+      },
+      clean: {
+        docpad: ['.docpad.db','out'],
+        bower: ['bower_components','./src/files/vendor'],
+        node: ['node_modules']
+      }
+    });
+
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('default','');
+};
--- docpad.coffee
+++ docpad.coffee
@@ -3,7 +3,25 @@
 
 # Define the DocPad Configuration
 docpadConfig = {
-	# ...
+  templateData:
+    site:
+      title: "Title here"
+      description: "description here"
+      projectName: "Project Name"
+      outdatedWarning: """
+      Votre version de navigateur <strong>n'est pas à jour</strong>.
+       Veuillez <a href="http://browsehappy.com/">installer une version
+       plus récente</a> de votre navigateur ou
+       <a href="http://www.google.com/chromeframe/?redirect=true">
+       installer le plugin Google Chrome Frame</a>
+       pour améliorer votre navigation sur internet.
+      """
+  watchOptions:
+    preferredMethods: ['watchFile','watch']
+	plugins:
+    grunt:
+      generateBefore: ['bower:install']
+      writeAfter: false
 }
 
 # Export the DocPad Configuration
